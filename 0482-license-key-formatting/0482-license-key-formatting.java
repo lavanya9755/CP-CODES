f9784650dc1184adc5efbfda61237253577ebad7
lavanya chawla <133964850+lavanya9755@users.noreply.github.com>
2025-05-12 22:02:56 +0530

    Time: 248 ms (5.09%), Space: 45.7 MB (12.83%) - LeetHub

---
+++ 0482-license-key-formatting/0482-license-key-formatting.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String licenseKeyFormatting(String s, int k) {
+        int n = s.length();
+
+        int cnt = 0; 
+        String temp = ""; //google question
+
+        for(int i=0;i<n;i++){
+            if(s.charAt(i)!='-'){
+                cnt++;
+                temp+=s.charAt(i);
+            }
+        }
+
+        int first_dash = cnt%k; //where the first dash has to be kept
+
+        String str = ""; //answer string
+        
+        int c = 0; //count to put dash when this value becomes equal to k
+
+        for(int i=0;i<cnt;i++){
+            if(i!=0 && (i==first_dash || c==k)){
+                str+='-';
+                str+=temp.charAt(i);
+                c=0;
+            }
+            else str = str + temp.charAt(i);
+            c++;
+        }
+        
+        return str.toUpperCase();
+    }
+}
