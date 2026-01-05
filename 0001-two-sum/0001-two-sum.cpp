b4f8a4162855137104fce9c42ee19b2bef2a2545
lavanya chawla <133964850+lavanya9755@users.noreply.github.com>
2026-01-05 21:35:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        for(int i=0; i<nums.size(); i++){
-            for(int j = i+1; j<nums.size();j++){
-                if(nums[i]+nums[j]== target){
-                    return {i,j};
-                }
-            }
+    void calc(vector<int> nums,int target,int i,int j,vector<int> &s){
+        if(i>=nums.size() || j<0){
+            return;
         }
-         return {};
+        if(nums[i]+nums[j]==target){
+            s.push_back(i);
+            s.push_back(j);
+            return;
+        }
+        calc(nums,target,i+1,j,s);
+        calc(nums,target,i,j-1,s);
+    }
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int i=0;
+        int j=nums.size()-1;
+        vector<int> s;
+        calc(nums,target,i,j,s);
+        return {s[0],s[1]};
     }
 };
